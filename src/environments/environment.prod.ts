8662f6334da56d71761f6b250977283c5d265f23
karlos <sky_karlos@hotmail.com>
2018-11-02 17:20:30 -0500

    chats css

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCQgCJLwliIZaYf5M5cJeaJS8acut6HCws',
+    authDomain: 'alex-b6df6.firebaseapp.com',
+    databaseURL: 'https://alex-b6df6.firebaseio.com',
+    projectId: 'alex-b6df6',
+    storageBucket: 'alex-b6df6.appspot.com',
+    messagingSenderId: '665097436063'
+  }
 };
