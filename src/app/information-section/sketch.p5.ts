4240891574788cb8e6adff5da9e78513f06ee478
Carlos Andres Gomez Vallejo <1107513806@academico.icesi.edu.co>
2018-11-19 18:13:32 -0500

    puntos de interes

--- src/app/information-section/sketch.p5.ts
+++ src/app/information-section/sketch.p5.ts
@@ -9,16 +9,28 @@ export function createSketch(width: number, height: number) {
   return function sketch(p: Ip5WithCustomAtributes) {
     let sketchWidth: number = width;
     let sketchHeight: number = height;
+
+    let headPointPos: p5.Vector;
+    let eyePointPos: p5.Vector;
+    let heardPointPos: p5.Vector;
+
     let puntos: PuntoPalpitante[];
 
     p.setup = function() {
       puntos = [];
 
+      headPointPos = p.createVector(sketchWidth / 2, sketchHeight / 3.5);
+      eyePointPos = p.createVector(sketchWidth / 1.7, sketchHeight / 2.5);
+      heardPointPos = p.createVector(sketchWidth / 1.9, sketchHeight / 2);
+
       p.createCanvas(sketchWidth, sketchHeight);
       p.frameRate(30);
-      p.rectMode(p.CENTER);
+      p.rectMode(p.CORNER);
+      p.ellipseMode(p.CENTER);
 
-      puntos.push(new PuntoPalpitante(sketchWidth / 10, sketchHeight / 10,  sketchWidth, sketchHeight, p));
+      puntos.push(new PuntoPalpitante(headPointPos.x, headPointPos.y,  sketchWidth, sketchHeight, p));
+      puntos.push(new PuntoPalpitante(eyePointPos.x, eyePointPos.y,  sketchWidth, sketchHeight, p));
+      puntos.push(new PuntoPalpitante(heardPointPos.x, heardPointPos.y,  sketchWidth, sketchHeight, p));
 
     };
 
@@ -33,8 +45,14 @@ export function createSketch(width: number, height: number) {
       sketchWidth = widthR;
       sketchHeight = heightR;
 
+      headPointPos.set(sketchWidth / 2, sketchHeight / 3.5);
+      eyePointPos.set(sketchWidth / 1.7, sketchHeight / 2.5);
+      heardPointPos.set(sketchWidth / 1.9, sketchHeight / 2);
+
       if (puntos) {
-        puntos[0].onResizeupdatePos(sketchWidth / 10, sketchHeight / 10);
+        puntos[0].onResizeupdatePos(headPointPos.x, headPointPos.y);
+        puntos[1].onResizeupdatePos(eyePointPos.x, eyePointPos.y);
+        puntos[2].onResizeupdatePos(heardPointPos.x, heardPointPos.y);
       }
 
       p.resizeCanvas(sketchWidth, sketchHeight);
@@ -58,6 +76,9 @@ class PuntoPalpitante {
 
   private canvasWidth: number;
   private canvasHeight: number;
+
+  private textRect: textBox;
+
   private pInstance: p5;
 
   constructor(_xPos: number, _yPos: number, _canvasWidth: number, _canvasHeight: number, _p: p5) {
@@ -75,6 +96,10 @@ class PuntoPalpitante {
     this.fillOpacityTwo = 255;
 
     this.pInstance = _p;
+  }
+
+  createTextRect() {
+
   }
 
   update() {
@@ -110,5 +135,32 @@ class PuntoPalpitante {
       this.fillOpacityTwo = 255;
     }
   }
+
+}
+
+class textBox {
+  private position: p5.Vector; 
+
+  private boxWidth: number;
+  private boxHeight: number;
+
+  private text: string;
+
+  private p5Instance: p5;
+
+
+constructor(_position: p5.Vector, _boxWidth: number, _boxHeight: number, _text: string, _p5Instance: p5){
+  this.position = _position;
+  this.boxWidth = _boxWidth;
+  this.boxHeight = _boxHeight;
+  this.text = _text;
+  this.p5Instance = _p5Instance;
+}
+
+update() {
+  this.p5Instance.fill(155,155,255);
+  this.p5Instance.rect(this.position.x, this.position.y, this.boxWidth, this.boxHeight);
+}
+
 
 }
